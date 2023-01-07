3637180f98d09f895eb584e07c049871fea28ceb
bsv3243 <bsv3243@naver.com>
2023-01-07 21:41:11 +0900

    initial commit

---
+++ src/main/java/seong/onlinestudy/Hello.java
@@ -0,0 +1,15 @@
+package seong.onlinestudy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+}
