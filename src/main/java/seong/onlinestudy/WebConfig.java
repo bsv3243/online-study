5ad1781af41894f565bdb1c6c486156fe34218f1
hseong3243 <hseong3243@gmail.com>
2023-01-09 22:35:26 +0900

    feat: 세션 인증으로 변경

--- src/main/java/seong/onlinestudy/WebConfig.java
+++ src/main/java/seong/onlinestudy/WebConfig.java
@@ -4,15 +4,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import seong.onlinestudy.interceptor.AuthenticationInterceptor;
+import seong.onlinestudy.interceptor.LoginInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /* jwt 검증 인터셉터
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthenticationInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/v1/login", "/api/v1/logout", "/api/v1/members");
+    }
+    */
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v1/login, /api/v1/logout", "/api/v1/members");
     }
 }
--- src/main/java/seong/onlinestudy/controller/LoginController.java
+++ src/main/java/seong/onlinestudy/controller/LoginController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import seong.onlinestudy.domain.Member;
 import seong.onlinestudy.request.LoginRequest;
 import seong.onlinestudy.service.LoginService;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -23,14 +26,33 @@ public class LoginController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public String login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
+    public String login(@RequestBody @Valid LoginRequest loginRequest,
+                        HttpServletRequest request, HttpServletResponse response) {
 
+        /* JWT 사용 로그인
         String token = "Barer " + loginService.createToken(loginRequest);
         Cookie cookie = new Cookie("AUTHORIZATION", URLEncoder.encode(token, StandardCharsets.UTF_8));
         cookie.setHttpOnly(true);
 
         log.info("토큰이 발급되었습니다. username={}", loginRequest.getUsername());
         response.addCookie(cookie);
+         */
+
+        Member loginMember = loginService.login(loginRequest);
+        HttpSession session = request.getSession();
+
+        session.setAttribute("LOGIN_MEMBER", loginMember);
+
+        return "ok";
+    }
+
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
 
         return "ok";
     }
