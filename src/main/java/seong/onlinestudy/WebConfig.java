5aefeb8225b04dd1fffb20802f2743b26ca3d0b1
hseong3243 <hseong3243@gmail.com>
2023-02-18 16:19:25 +0900

    swagger 추가

--- src/main/java/seong/onlinestudy/WebConfig.java
+++ src/main/java/seong/onlinestudy/WebConfig.java
@@ -1,12 +1,13 @@
 package seong.onlinestudy;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import seong.onlinestudy.interceptor.AuthenticationInterceptor;
 import seong.onlinestudy.interceptor.LoginInterceptor;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     /* jwt 검증 인터셉터
@@ -19,11 +20,19 @@ public class WebConfig implements WebMvcConfigurer {
     }
     */
 
+    /*
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/v1/login, /api/v1/logout", "/api/v1/members");
+    }
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081");
     }
 }
--- src/main/java/seong/onlinestudy/controller/GroupController.java
+++ src/main/java/seong/onlinestudy/controller/GroupController.java
@@ -1,5 +1,7 @@
 package seong.onlinestudy.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ import java.util.List;
 
 import static seong.onlinestudy.SessionConst.*;
 
+@Api(tags = "GroupController")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -25,6 +28,7 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    @Operation(summary = "그룹 생성(인증)")
     @PostMapping("/groups")
     public Long createGroup(@RequestBody @Valid GroupCreateRequest createRequest,
                             @SessionAttribute(name = LOGIN_MEMBER)Member loginMember) {
@@ -34,6 +38,7 @@ public class GroupController {
         return groupId;
     }
 
+    @Operation(summary = "그룹 가입(인증)")
     @PostMapping("/groups/{groupId}")
     public Long joinGroup(@PathVariable Long groupId,
                           @SessionAttribute(name = LOGIN_MEMBER) Member loginMember) {
@@ -42,6 +47,7 @@ public class GroupController {
         return joinedGroupId;
     }
 
+    @Operation(summary = "그룹 리스트 반환")
     @GetMapping("/groups")
     public Result<List<GroupDto>> getGroups(@RequestParam(defaultValue = "0") int page,
                                          @RequestParam(defaultValue = "10") int size,
@@ -55,6 +61,7 @@ public class GroupController {
         return result;
     }
 
+    @Operation(summary = "그룹 1개 반환")
     @GetMapping("/groups/{id}")
     public GroupDto getGroup(@PathVariable Long id) {
         GroupDto group = groupService.getGroup(id);
@@ -62,6 +69,7 @@ public class GroupController {
         return group;
     }
 
+    @Operation(summary = "그룹 삭제(인증)")
     @DeleteMapping("/groups/{id}")
     public String deleteGroup(@PathVariable Long id, @SessionAttribute(name = LOGIN_MEMBER) Member loginMember) {
         if(loginMember == null) {
