f45070fa67ebc0118ed77a2ecc6084821daba517
bsv3243 <bsv3243@naver.com>
2023-03-01 20:54:54 +0900

    feat: 댓글 수정 API 추가

--- src/main/java/seong/onlinestudy/service/CommentService.java
+++ src/main/java/seong/onlinestudy/service/CommentService.java
@@ -7,8 +7,10 @@ import org.springframework.transaction.annotation.Transactional;
 import seong.onlinestudy.domain.Comment;
 import seong.onlinestudy.domain.Member;
 import seong.onlinestudy.domain.Post;
+import seong.onlinestudy.exception.UnAuthorizationException;
 import seong.onlinestudy.repository.PostRepository;
 import seong.onlinestudy.request.CommentCreateRequest;
+import seong.onlinestudy.request.CommentUpdateRequest;
 
 import java.util.NoSuchElementException;
 
@@ -30,6 +32,25 @@ public class CommentService {
         comment.setMemberAndPost(loginMember, post);
         log.info("댓글이 작성되었습니다. postId={}, commentId={}, memberId={}",
                 post.getId(), comment.getId(), loginMember.getId());
+
+        return comment.getId();
+    }
+
+    @Transactional
+    public Long updateComment(Long commentId, CommentUpdateRequest request, Member loginMember) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 댓글입니다."));
+
+        //작성자 정보가 같지 않으면
+        if (!comment.getMember().getId().equals(loginMember.getId())) {
+            throw new UnAuthorizationException("댓글 수정 권한이 없습니다.");
+        }
+
+        String oldContent = comment.getContent();
+
+        comment.update(request);
+        log.info("댓글이 수정되었습니다. commentId={}, oldContent={}, newContent={}",
+                comment.getId(), oldContent, comment.getContent());
 
         return comment.getId();
     }
