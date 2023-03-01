69f3f2adb5d5a97b537a685c1fdc4ca60444ee4e
bsv3243 <bsv3243@naver.com>
2023-03-01 23:40:49 +0900

    feat: 그룹 단건 조회 API 추가

--- src/main/java/seong/onlinestudy/repository/GroupRepository.java
+++ src/main/java/seong/onlinestudy/repository/GroupRepository.java
@@ -2,11 +2,21 @@ package seong.onlinestudy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import seong.onlinestudy.domain.Group;
 import seong.onlinestudy.dto.GroupMemberDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long>, GroupRepositoryCustom {
+
+    /**
+     * Group 의 Id와 groupId가 일치하는 목록을 조회한다.
+     * @param groupId
+     * @return GroupMember, Member 를 페치 조인한 리스트를 반환
+     */
+    @Query("select g from Group g join fetch g.groupMembers gm join fetch gm.member where g.id=:groupId")
+    Optional<Group> findGroupWithMembers(@Param("groupId") Long groupId);
 
 }
