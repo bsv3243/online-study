2d87087f6aa6e6599fc2d79d811bb8f996001cff
hseong3243 <hseong3243@gmail.com>
2023-03-01 22:54:48 +0900

    게시글 작성 메서드 미반영분 추가

--- src/main/java/seong/onlinestudy/repository/GroupRepository.java
+++ src/main/java/seong/onlinestudy/repository/GroupRepository.java
@@ -1,11 +1,17 @@
 package seong.onlinestudy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import seong.onlinestudy.domain.Group;
 import seong.onlinestudy.dto.GroupMemberDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long>, GroupRepositoryCustom {
+
+    @Query("select g from Group g join fetch g.groupMembers gm join fetch gm.member where g.id=:groupId")
+    Optional<Group> findGroupWithMembers(@Param("groupId") Long groupId);
 
 }
