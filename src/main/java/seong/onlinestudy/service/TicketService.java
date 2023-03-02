4de76e61890ce29c0acffd89483493407a889e19
hseong3243 <hseong3243@gmail.com>
2023-03-02 19:40:47 +0900

    티켓 생성 메서드 티켓 중복 발급 검증 추가

--- src/main/java/seong/onlinestudy/service/TicketService.java
+++ src/main/java/seong/onlinestudy/service/TicketService.java
@@ -3,19 +3,21 @@ package seong.onlinestudy.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import seong.onlinestudy.domain.Group;
-import seong.onlinestudy.domain.Member;
-import seong.onlinestudy.domain.Study;
-import seong.onlinestudy.domain.Ticket;
+import seong.onlinestudy.domain.*;
 import seong.onlinestudy.dto.TicketDto;
-import seong.onlinestudy.exception.InvalidAuthorizationException;
+import seong.onlinestudy.exception.DuplicateElementException;
+import seong.onlinestudy.exception.UnAuthorizationException;
 import seong.onlinestudy.repository.GroupRepository;
 import seong.onlinestudy.repository.StudyRepository;
 import seong.onlinestudy.repository.TicketRepository;
 import seong.onlinestudy.request.TicketCreateRequest;
 import seong.onlinestudy.request.TicketUpdateRequest;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+
+import static seong.onlinestudy.domain.TicketStatus.REST;
+import static seong.onlinestudy.domain.TicketStatus.STUDY;
 
 @Slf4j
 @Service
@@ -26,14 +28,19 @@ public class TicketService {
     private final StudyRepository studyRepository;
     private final GroupRepository groupRepository;
 
-    public Long createTicket(TicketCreateRequest createTicketRequest, Member loginMember) {
-        Study findStudy = studyRepository.findById(createTicketRequest.getStudyId())
+    public Long createTicket(TicketCreateRequest request, Member loginMember) {
+        Study findStudy = studyRepository.findById(request.getStudyId())
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 스터디입니다."));
 
-        Group findGroup = groupRepository.findById(createTicketRequest.getGroupId())
+        Group findGroup = groupRepository.findById(request.getGroupId())
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 그룹입니다."));
 
-        Ticket ticket = Ticket.createTicket(loginMember, findStudy, findGroup);
+        ticketRepository.findByMemberAndTicketStatusIn(loginMember, List.of(STUDY, REST))
+                .ifPresent(ticket -> {
+                    throw new DuplicateElementException("이전에 발급받은 티켓이 존재합니다.");
+                });
+
+        Ticket ticket = Ticket.createTicket(request, loginMember, findStudy, findGroup);
         ticketRepository.save(ticket);
 
         return ticket.getId();
@@ -45,7 +52,7 @@ public class TicketService {
 
         //티켓의 회원 ID와 일치하지 않으면
         if(!findTicket.getMember().getId().equals(loginMember.getId())) {
-            throw new InvalidAuthorizationException("권한이 없습니다.");
+            throw new UnAuthorizationException("권한이 없습니다.");
         }
 
         findTicket.updateStatus(updateTicketRequest);
