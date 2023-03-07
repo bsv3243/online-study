2e7382e96a95c65b9333abb93180f8f0cd30e6ba
bsv3243 <bsv3243@naver.com>
2023-03-07 22:38:47 +0900

    @NotNull 빠진 곳 추가

--- src/main/java/seong/onlinestudy/request/LoginRequest.java
+++ src/main/java/seong/onlinestudy/request/LoginRequest.java
@@ -2,16 +2,19 @@ package seong.onlinestudy.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 public class LoginRequest {
 
+    @NotNull(message = "아이디는 6자 이상, 20자 이하여야 합니다.")
     @Size(min = 6, max = 20, message = "아이디는 6자 이상, 20자 이하여야 합니다.")
     @Pattern(regexp = "[a-zA-Z0-9]+", message = "아이디는 영문과 숫자로 구성되어야 합니다.")
     private String username;
 
+    @NotNull(message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
     @Size(min = 6, max = 20, message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*\\\\W).{6,20}$",
             message = "비밀번호는 영문, 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
--- src/main/java/seong/onlinestudy/request/StudyCreateRequest.java
+++ src/main/java/seong/onlinestudy/request/StudyCreateRequest.java
@@ -2,8 +2,11 @@ package seong.onlinestudy.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class StudyCreateRequest {
 
+    @NotNull(message = "스터디명은 필수입니다.")
     private String name;
 }
