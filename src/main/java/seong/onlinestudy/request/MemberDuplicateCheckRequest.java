9fe88b50eba394444ee9350066fdbeac6533f3cc
hseong3243 <hseong3243@gmail.com>
2023-03-07 23:02:04 +0900

    feat: 회원 아이디 중복 체크 추가

--- src/main/java/seong/onlinestudy/request/MemberDuplicateCheckRequest.java
+++ src/main/java/seong/onlinestudy/request/MemberDuplicateCheckRequest.java
@@ -7,16 +7,10 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
-public class LoginRequest {
+public class MemberDuplicateCheckRequest {
 
     @NotNull(message = "아이디는 6자 이상, 20자 이하여야 합니다.")
     @Size(min = 6, max = 20, message = "아이디는 6자 이상, 20자 이하여야 합니다.")
     @Pattern(regexp = "[a-zA-Z0-9]+", message = "아이디는 영문과 숫자로 구성되어야 합니다.")
     private String username;
-
-    @NotNull(message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
-    @Size(min = 6, max = 20, message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,20}$",
-            message = "비밀번호는 영문, 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
-    private String password;
 }
--- src/test/java/seong/onlinestudy/controller/MemberControllerTest.java
+++ src/test/java/seong/onlinestudy/controller/MemberControllerTest.java
@@ -2,42 +2,57 @@ package seong.onlinestudy.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import seong.onlinestudy.request.MemberCreateRequest;
+import seong.onlinestudy.service.MemberService;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
     @Autowired
     MockMvc mvc;
-    @Autowired
+
     ObjectMapper mapper;
+
+    @MockBean
+    MemberService memberService;
+
+    public MemberControllerTest() {
+        this.mapper = new ObjectMapper();
+    }
 
     @Test
     void addMember_Success() throws Exception {
         //given
         MemberCreateRequest request = new MemberCreateRequest();
         request.setUsername("test123");
-        request.setPassword("test123");
-        request.setNickname("test");
+        request.setPassword("test123!");
+        request.setNickname("test12");
 
+        given(memberService.addMember(any())).willReturn(1L);
         //when
-        mvc.perform(post("/api/member/v1/addMember")
+        mvc.perform(post("/api/v1/members")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
@@ -48,18 +63,41 @@ class MemberControllerTest {
     }
 
     @Test
-    void addMember_fail() throws Exception {
+    void addMember_비밀번호검증실패() throws Exception {
         //given
         MemberCreateRequest request = new MemberCreateRequest();
         request.setUsername("test123");
-        request.setPassword("test"); //최소 길이보다 짧은 패스워드
+        request.setPassword("test123"); //특수문자 미포함
         request.setNickname("test");
 
         //when
-        mvc.perform(post("/api/member/v1/addMember")
+        mvc.perform(post("/api/v1/members")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
+                .andExpect(rs -> {
+                    assertThat(rs.getResolvedException())
+                            .isInstanceOf(MethodArgumentNotValidException.class);
+                })
+                .andDo(print());
+
+        //then
+    }
+
+    @Test
+    void addMember_모두Null() throws Exception {
+        //given
+        MemberCreateRequest request = new MemberCreateRequest();
+
+        //when
+        mvc.perform(post("/api/v1/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(rs -> {
+                    assertThat(rs.getResolvedException())
+                            .isInstanceOf(MethodArgumentNotValidException.class);
+                })
                 .andDo(print());
 
         //then
