d0dd29443f7a6ac334c5cafa612c43c58681eff6
bsv3243 <bsv3243@naver.com>
2023-03-07 21:28:23 +0900

    vaildation message 추가

--- src/main/java/seong/onlinestudy/request/MemberCreateRequest.java
+++ src/main/java/seong/onlinestudy/request/MemberCreateRequest.java
@@ -3,19 +3,19 @@ package seong.onlinestudy.request;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 public class MemberCreateRequest {
 
 //    @Range(min = 6, max = 20, message = "아이디는 6자 이상, 20자 이하여야 합니다.")
     @Size(min = 6, max = 20, message = "아이디는 6자 이상, 20자 이하여야 합니다.")
+    @Pattern(regexp = "[a-zA-Z0-9]+", message = "아이디는 영문과 숫자로 구성되어야 합니다.")
     private String username;
 
     @Size(min = 6, max = 20, message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*\\\\W).{6,20}$",
+            message = "비밀번호는 영문, 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
     private String password;
 
     @Size(min = 2, max = 12, message = "닉네임은 2자 이상, 12자 이아혀야 합니다.")
--- src/main/java/seong/onlinestudy/request/PostCreateRequest.java
+++ src/main/java/seong/onlinestudy/request/PostCreateRequest.java
@@ -3,14 +3,22 @@ package seong.onlinestudy.request;
 import lombok.Data;
 import seong.onlinestudy.domain.PostCategory;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class PostCreateRequest {
 
+    @NotNull(message = "제목은 공란일 수 없습니다.")
     private String title;
+
+    @NotNull(message = "본문은 공란일 수 없습니다.")
     private String content;
+
+    @NotNull(message = "카테고리는 필수입니다.")
     private PostCategory category;
+
     private List<Long> studyIds;
     private Long groupId;
 }
