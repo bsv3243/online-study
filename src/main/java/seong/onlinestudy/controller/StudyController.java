15cf954d6557f0fee20803a165fe709c3c9f2091
bsv3243 <bsv3243@naver.com>
2023-03-09 02:17:30 +0900

    feat: 스터디 목록 조회 API에 회원의 최근 스터디 목록 조회 추가

--- src/main/java/seong/onlinestudy/controller/StudyController.java
+++ src/main/java/seong/onlinestudy/controller/StudyController.java
@@ -3,13 +3,16 @@ package seong.onlinestudy.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+import seong.onlinestudy.domain.Member;
 import seong.onlinestudy.dto.StudyDto;
 import seong.onlinestudy.request.StudyCreateRequest;
-import seong.onlinestudy.request.StudySearchCond;
+import seong.onlinestudy.request.StudiesGetRequest;
 import seong.onlinestudy.service.StudyService;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static seong.onlinestudy.SessionConst.LOGIN_MEMBER;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +29,10 @@ public class StudyController {
     }
 
     @GetMapping("/studies")
-    public Result<List<StudyDto>> getStudies(@Valid StudySearchCond searchCond) {
-        Page<StudyDto> studies = studyService.getStudies(searchCond);
+    public Result<List<StudyDto>> getStudies(@Valid StudiesGetRequest searchCond,
+                                             @SessionAttribute(value = LOGIN_MEMBER, required = false) Member loginMember) {
+
+        Page<StudyDto> studies = studyService.getStudies(searchCond, loginMember);
         Result<List<StudyDto>> result = new Result<>("200", studies.getContent());
         result.setPageInfo(studies);
 
