bd875240013c52c8f1ff3a64fee2a64734c7626e
hseong3243 <hseong3243@gmail.com>
2023-03-09 11:13:06 +0900

    feat: 회원의 studyDto 리스트를 가져올 때 ticket 정보를 포함하도록 변경, batchSize 설정 추가

--- src/test/java/seong/onlinestudy/repository/StudyRepositoryTest.java
+++ src/test/java/seong/onlinestudy/repository/StudyRepositoryTest.java
@@ -6,17 +6,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.util.ReflectionTestUtils;
-import seong.onlinestudy.MyUtils;
 import seong.onlinestudy.domain.*;
+import seong.onlinestudy.dto.StudyDto;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static seong.onlinestudy.MyUtils.*;
 import static seong.onlinestudy.domain.TicketStatus.STUDY;
@@ -66,7 +66,7 @@ class StudyRepositoryTest {
 
         //when
         LocalDateTime now = LocalDateTime.now();
-        /*
+
         List<Study> findStudies = em.createQuery("select s from Study s" +
                         " join s.tickets t on t.member = :member and t.startTime >= :startTime and t.startTime < :endTime" +
                         " group by s.id" +
@@ -75,13 +75,81 @@ class StudyRepositoryTest {
                 .setParameter("startTime", now.minusDays(3))
                 .setParameter("endTime", now.plusDays(3))
                 .getResultList();
-         */
+
+/*        Page<Study> studiesByMember
+                = studyRepository.findStudiesByMember(members.get(0), now.minusDays(3), now.plusDays(3), PageRequest.of(0, 2));
+        List<Study> findStudies = studiesByMember.getContent();*/
+
+        //then
+        assertThat(findStudies).containsExactlyElementsOf(List.of(studies.get(4), studies.get(5)));
+    }
+
+    @Test
+    void findStudiesWithMember_티켓페치조인() {
+        //given
+        List<Member> members = createMembers(10);
+        memberRepository.saveAll(members);
+
+        List<Study> studies = createStudies(6);
+        studyRepository.saveAll(studies);
+
+        Group group = createGroup("group", 30, members.get(0));
+        groupRepository.save(group);
+
+        Ticket ticket = createTicket(STUDY, members.get(0), studies.get(4), group);
+        ticketRepository.save(ticket);
+        setTicketEnd(ticket, 3600);
+
+        Ticket ticket1 = createTicket(STUDY, members.get(0), studies.get(5), group);
+        ticketRepository.save(ticket1);
+
+        Ticket ticket2 = createTicket(STUDY, members.get(0), studies.get(4), group);
+        ticketRepository.save(ticket2);
+        setTicketEnd(ticket2, 2800);
+
+        List<Ticket> tickets = new ArrayList<>();
+        for(int i=1; i<members.size(); i++) {
+            GroupMember.createGroupMember(members.get(i), GroupRole.USER);
+            tickets.add(createTicket(STUDY, members.get(i), studies.get(i%4), group));
+        }
+        ticketRepository.saveAll(tickets);
+
+
+        //when
+        LocalDateTime now = LocalDateTime.now();
+
+/*        List<Study> findStudies = em.createQuery("select distinct s from Study s" +
+                        " join fetch s.tickets t " +
+                        " where t.member = :member and t.startTime >= :startTime and t.startTime < :endTime" +
+                        " order by t.startTime desc", Study.class)
+                .setParameter("member", members.get(0))
+                .setParameter("startTime", now.minusDays(3))
+                .setParameter("endTime", now.plusDays(3))
+                .getResultList();*/
+
         Page<Study> studiesByMember
                 = studyRepository.findStudiesByMember(members.get(0), now.minusDays(3), now.plusDays(3), PageRequest.of(0, 2));
         List<Study> findStudies = studiesByMember.getContent();
 
         //then
-        Assertions.assertThat(findStudies).containsExactlyElementsOf(List.of(studies.get(4), studies.get(5)));
+        assertThat(findStudies).containsExactlyElementsOf(List.of(studies.get(4), studies.get(5)));
+        List<StudyDto> studyDtos = findStudies.stream().map(study -> {
+            StudyDto studyDto = StudyDto.from(study);
+            studyDto.setStudyTime(study);
+
+            return studyDto;
+        }).collect(Collectors.toList());
+
+        //티켓이 만료되어 활성화된 시간이 0보다 큰 경우
+        assertThat(studyDtos).anySatisfy(studyDto -> {
+            assertThat(studyDto.getStudyTime()).isEqualTo(3600 + 2800);
+            assertThat(studyDto.getEndTime()).isEqualTo(ticket.getEndTime().format(DateTimeFormatter.ISO_DATE_TIME));
+        });
+        //티켓이 만료되지 않아 활성화된 시간이 0인 경우
+        assertThat(studyDtos).anySatisfy(studyDto -> {
+            assertThat(studyDto.getStudyTime()).isEqualTo(0);
+            assertThat(studyDto.getEndTime()).isEqualTo(ticket1.getStartTime().format(DateTimeFormatter.ISO_DATE_TIME));
+        });
     }
 
 }
