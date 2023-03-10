17849b28cc3b86b4082e1f1ed4ef1dd290637d73
hseong3243 <hseong3243@gmail.com>
2023-03-11 00:16:39 +0900

    게시글 dto, 댓글 dto 생성 시간 필드 추가

--- src/main/java/seong/onlinestudy/dto/CommentDto.java
+++ src/main/java/seong/onlinestudy/dto/CommentDto.java
@@ -3,12 +3,15 @@ package seong.onlinestudy.dto;
 import lombok.Data;
 import seong.onlinestudy.domain.Comment;
 
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class CommentDto {
 
     private Long commentId;
     private String content;
     private MemberDto member;
+    private String createdAt;
     private Long postId;
 
     public static CommentDto from(Comment comment) {
@@ -16,6 +19,7 @@ public class CommentDto {
         commentDto.commentId = comment.getId();
         commentDto.content = comment.getContent();
         commentDto.member = MemberDto.from(comment.getMember());
+        commentDto.createdAt = comment.getCreatedAt().format(DateTimeFormatter.ISO_DATE_TIME);
         commentDto.postId = comment.getPost().getId();
 
         return commentDto;
