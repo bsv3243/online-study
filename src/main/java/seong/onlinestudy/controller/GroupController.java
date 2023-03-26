cc6945d8ae4f1f80e1bfc0d853fb10cd82f8df13
hseong3243 <hseong3243@gmail.com>
2023-03-26 19:11:23 +0900

    feat: spring rest docs 종속성 추가, swagger 삭제
    api 문서화를 위한 spring rest docs 종속성 추가 및 기존의 swagger 종속성과 관련 코드를 삭제

--- src/main/java/seong/onlinestudy/controller/GroupController.java
+++ src/main/java/seong/onlinestudy/controller/GroupController.java
@@ -1,7 +1,5 @@
 package seong.onlinestudy.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +17,6 @@ import java.util.List;
 
 import static seong.onlinestudy.SessionConst.*;
 
-@Api(tags = "GroupController")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -27,7 +24,6 @@ public class GroupController {
 
     private final GroupService groupService;
 
-    @Operation(summary = "그룹 생성(인증)")
     @PostMapping("/groups")
     public Result<Long> createGroup(@RequestBody @Valid GroupCreateRequest createRequest,
                             @SessionAttribute(name = LOGIN_MEMBER, required = false)Member loginMember) {
@@ -40,7 +36,6 @@ public class GroupController {
         return new Result<>("201", groupId);
     }
 
-    @Operation(summary = "그룹 가입(인증)")
     @PostMapping("/group/{groupId}/join")
     public Result<Long> joinGroup(@PathVariable Long groupId,
                           @SessionAttribute(name = LOGIN_MEMBER, required = false) Member loginMember) {
@@ -65,7 +60,6 @@ public class GroupController {
         return new Result<>("200", "ok");
     }
 
-    @Operation(summary = "그룹 리스트 반환")
     @GetMapping("/groups")
     public Result<List<GroupDto>> getGroups(@Valid GroupsGetRequest request) {
         Page<GroupDto> groups = groupService.getGroups(request);
@@ -76,7 +70,6 @@ public class GroupController {
         return result;
     }
 
-    @Operation(summary = "그룹 1개 반환")
     @GetMapping("/group/{id}")
     public Result<GroupDto> getGroup(@PathVariable Long id) {
         GroupDto group = groupService.getGroup(id);
@@ -84,7 +77,6 @@ public class GroupController {
         return new Result<>("200", group);
     }
 
-    @Operation(summary = "그룹 삭제(인증)")
     @DeleteMapping("/group/{id}")
     public Result<String> deleteGroup(@PathVariable Long id,
                                       @SessionAttribute(name = LOGIN_MEMBER, required = false) Member loginMember) {
