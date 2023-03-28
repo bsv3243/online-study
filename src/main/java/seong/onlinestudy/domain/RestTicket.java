d73e98f5f4d65cf78fbda9defa0a0fcdbabb3b18
hseong3243 <hseong3243@gmail.com>
2023-03-28 10:51:51 +0900

    modify: Ticket 테이블 정규화로 인한 엔티티 클래스 추가
    기존 Ticket 테이블의 ticketStatus 필드가 "REST" 인 경우 study 필드는 null을 가짐. 이에 따라 Ticket 테이블을 StudyTicket, RestTicket 으로 분리하고 상속관계 매핑을 이용함.

---
+++ src/main/java/seong/onlinestudy/domain/RestTicket.java
@@ -0,0 +1,25 @@
+package seong.onlinestudy.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@DiscriminatorValue(TicketStatus.Values.REST)
+@PrimaryKeyJoinColumn(name = "rest_ticket_id")
+public class RestTicket extends Ticket {
+
+    protected RestTicket() {
+    }
+
+    private RestTicket(Member member, Group group) {
+        super(member, group);
+    }
+
+    public static Ticket createRestTicket(Member member, Group group) {
+        RestTicket restTicket = new RestTicket(member, group);
+
+        return restTicket;
+    }
+}
--- src/main/java/seong/onlinestudy/domain/TicketStatus.java
+++ src/main/java/seong/onlinestudy/domain/TicketStatus.java
@@ -1,5 +1,19 @@
 package seong.onlinestudy.domain;
 
 public enum TicketStatus {
-    STUDY, REST, END
+    STUDY(Values.STUDY),
+    REST(Values.REST);
+
+    private String value;
+
+    TicketStatus(String value) {
+        if(!this.name().equals(value)) {
+            throw new IllegalArgumentException("name 값과 일치하지 않습니다.");
+        }
+    }
+
+    public static class Values {
+        public static final String STUDY = "STUDY";
+        public static final String REST = "REST";
+    }
 }
