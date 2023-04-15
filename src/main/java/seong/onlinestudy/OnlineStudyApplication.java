a52246a9216d408520f864232f3c7dccfdca3e41
hseong3243 <hseong3243@gmail.com>
2023-04-15 23:12:19 +0900

    remove: EnalbeRedisHttpSession 어노테이션 삭제
    해당 어노테이션 없이 application.yml 설정만으로 레디스 세션 설정 가능

--- src/main/java/seong/onlinestudy/OnlineStudyApplication.java
+++ src/main/java/seong/onlinestudy/OnlineStudyApplication.java
@@ -3,10 +3,8 @@ package seong.onlinestudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableScheduling
-@EnableRedisHttpSession
 @SpringBootApplication
 public class OnlineStudyApplication {
 
